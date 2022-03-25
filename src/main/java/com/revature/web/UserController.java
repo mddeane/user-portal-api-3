7739c6de343eae6e5c71f7441f6b6d037de0f1f8
Mark Deane <mddeane@yahoo.com>
2022-03-25 01:38:05 -0500

    added CORS filter

--- src/main/java/com/revature/web/UserController.java
+++ src/main/java/com/revature/web/UserController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.revature.service.UserService;
 
 @RestController
 @RequestMapping("/users") // available at http://localhost:5000/api/users
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class UserController {
 	
 	@Autowired
